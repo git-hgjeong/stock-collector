0bee7cd0c7bdcd09785293b2ded2fcdb8b9a22ff
git-hgjeong <code.hgjeong@gmail.com>
2023-06-29 20:04:54 +0900

    API Server로 데이터 전송

--- collector.py
+++ collector.py
@@ -5,6 +5,8 @@ from PyQt5.QtCore import *
 import pandas as pd
 from io import StringIO
 import time
+import requests
+import json
 
 is_64bits = sys.maxsize > 2**32
 if is_64bits:
@@ -49,7 +51,7 @@ class KiwoomAPI(QAxWidget):
     # 데이터요청(CommRqData) 콜백함수
     # sScrNo(화면번호), sRQName(사용자구분), sTrCode(Tran명), sRecordName(레코드명), sPreNext(연속조회 유무)
     def apiOnReceiveTrData(self, sScrNo, sRQName, sTrCode, sRecordName, sPreNext):
-        print(">> Receive Result:"+ sRQName)
+        #print(">> Receive Result:"+ sRQName)
         if sRQName == 'GET-BASIC-DATA':
             # 종목명	현재가	등락률	거래량	거래대금(백만)
             name = self.dynamicCall("GetCommData(QString, QString, int, QString)", sTrCode, sRQName, 0, "종목코드")
@@ -57,7 +59,7 @@ class KiwoomAPI(QAxWidget):
             rate = self.dynamicCall("GetCommData(QString, QString, int, QString)", sTrCode, sRQName, 0, "등락율")
             per = self.dynamicCall("GetCommData(QString, QString, int, QString)", sTrCode, sRQName, 0, "PER")
             tp = (name.lstrip(), price.lstrip(), rate.lstrip(), per.lstrip())
-            print(tp)
+            #print(tp)
             self.DATA_StockList.append(tp)
         elif sRQName == 'GET-DAY-DATA':
 
@@ -70,21 +72,21 @@ class KiwoomAPI(QAxWidget):
             #high_price = self.dynamicCall("GetCommData(QString, QString, int, QString)", sTrCode, sRQName, 0, "고가")
             #low_price = self.dynamicCall("GetCommData(QString, QString, int, QString)", sTrCode, sRQName, 0, "저가")
             tp = (code.lstrip(), price.lstrip(), volume.lstrip(), trade_price.lstrip(), date.lstrip())
-            print(tp)
+            #print(tp)
             self.DATA_DayTradeList.append(tp)
         self.data_event_loop.exit()
 
     def apiOnReceiveTrCondition(self, scrno, codelist, conditionname, nindex, nnext):
-        print(">> apiOnReceiveTrCondition")
+        #print(">> apiOnReceiveTrCondition")
         arr = codelist.split(';')
         arr.pop()
         self.DATA_StockCodeList = arr
         #print(arr)
         self.data_event_loop.exit()
     def apiOnReceiveConditionVer(self):
-        print(">> 조건식 데이터 조회 완료")
+        #print(">> 조건식 데이터 조회 완료")
         sConditionResult = self.dynamicCall("GetConditionNameList()")
-        print(sConditionResult)
+        #print(sConditionResult)
         # 세미콜론과 캐럿으로 구성된 CSV 형식으로 변환.
         csv_str = sConditionResult.replace(';', '\n').replace('^', ',')
         # 문자열을 DataFrame으로 변환.
@@ -97,7 +99,7 @@ class KiwoomAPI(QAxWidget):
     def getBasicData(self, stock_code):
         # 연속 요청시 씹히는 문제 sleep 처리
         time.sleep(0.2)
-        print(">> 데이터요청:"+ stock_code)
+        #print(">> 데이터요청:"+ stock_code)
         # 검색조건
         self.dynamicCall("SetInputValue(QString, QString)", "종목코드", stock_code)
         # 데이터 조회 실행
@@ -108,7 +110,7 @@ class KiwoomAPI(QAxWidget):
     def getDayData(self, stock_code):
         # 연속 요청시 씹히는 문제 sleep 처리
         time.sleep(0.2)
-        print(">> 데이터요청:"+ stock_code)
+        #print(">> 데이터요청:"+ stock_code)
         # 검색조건
         self.dynamicCall("SetInputValue(QString, QString)", "종목코드", stock_code)
         self.dynamicCall("SetInputValue(QString, QString)", "기준일자", None)
@@ -130,7 +132,7 @@ class KiwoomAPI(QAxWidget):
         df = self.DATA_Conditions[is_filter]
         code = df.at[0, 'code']
         is_result = self.dynamicCall("SendCondition(QString, QString, int, int)", "0156", sConditionName, code, 0)
-        print(is_result)
+        #print(is_result)
         if is_result == 1:
             self.data_event_loop = QEventLoop()
             self.data_event_loop.exec_()
@@ -140,17 +142,31 @@ class KiwoomAPI(QAxWidget):
                 #print(code)
                 self.getBasicData(code)
                 self.getDayData(code)
-            print("==========================================")
+            #print("==========================================")
             dfStock = pd.DataFrame(self.DATA_StockList, columns=['ticker','stock_name', 'change_price_rate', 'per'])
-            print(dfStock)
+            #print(dfStock)
             dfDayTrade = pd.DataFrame(self.DATA_DayTradeList, columns=['ticker', 'price', 'trading_volume', 'trading_amount', 'trading_date'])
-            print(dfDayTrade)
+            #print(dfDayTrade)
             dfMergeData = pd.merge(dfStock, dfDayTrade, left_on='ticker', right_on='ticker', how='outer')
-            print(dfMergeData.to_json(orient='records'))
+            requestData = dfMergeData.to_json(orient='records')
+            #print(requestData)
             dfMergeData.to_excel(dfMergeData.at[0, 'trading_date'] +'.xlsx')
 
         else:
             print("Fail getMyConditionData.")
+        # Transfer to API Server
+        self.addStockDataToApiServer(requestData)
+
+    def addStockDataToApiServer(self, requestData):
+        # URL
+        url = "http://dev592.cafe24.com/stock/api/add.php"
+        # headers
+        headers = {
+            "Content-Type": "application/json"
+        }
+        response = requests.post(url, headers=headers, data=requestData)
+        print("response: ", response)
+        print("response.text: ", response.text)
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
